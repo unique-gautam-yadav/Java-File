6ac8e12e8e1c6fdbbb0d9ef1c277a29e7bea5797
Gautamyadavglaian <gautam.gla_csd20@gla.ac.in>
2022-04-28 10:34:21 +0530

    Java File Completed

---
+++ Pro36_AppletLife.java
@@ -0,0 +1,28 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class Pro36_AppletLife extends Applet {
+    public void init() {
+        System.out.println("init Called");
+    }
+
+    public void start() {
+        System.out.println("Start called");
+    }
+
+    public void stop() {
+        System.out.println("Stop called");
+    }
+
+    public void destroy() {
+        System.out.println("Destroy called");
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Hello Applet !!", 150, 150);
+    }
+}
+/*
+ * <applet code = "Pro36_AppletLife" width = "300" height = "300">
+ * </applet>
+ */
